e2bec48a47ae5f7bd9587fd50ffcbc9e335d6a8a
Brian Schroer <schroer.brian@gmail.com>
2013-07-14 20:45:46 -0700

    Add @user variable to city controller

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -29,7 +29,7 @@ class CitiesController < ApplicationController
   def city
   	client = Soundcloud.new(:client_id => "e9e8fbf8ac2f57eb0f54519af9c2f22e") 
   	@track = client.get("/users/" + params["user"] + "/tracks" )
-
+  	@user = client.get("/users/" + params["user"])
   end
 
   def track
