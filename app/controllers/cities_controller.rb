72937adba648b94f8ae93d626dbf15090840dbf1
Brian Schroer <schroer.brian@gmail.com>
2013-07-18 14:46:34 -0700

    refactoring, simple stuff

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -11,7 +11,7 @@ class CitiesController < ApplicationController
 		#here's where i call the soundcloud api
 		client = Soundcloud.new(:client_id => "e9e8fbf8ac2f57eb0f54519af9c2f22e")
 
-		tracks = client.get('/tracks', :limit => 4)
+		tracks = client.get('/tracks', :limit => 5)
 		tracks.each do |track|
 			user = client.get('/users/' + track.user_id.to_s)
 
@@ -27,18 +27,5 @@ class CitiesController < ApplicationController
 			end
 		end
 	end
-
-  
-
-#this is stuff i may not need
-  def city
-  	client = Soundcloud.new(:client_id => "e9e8fbf8ac2f57eb0f54519af9c2f22e") 
-  	@track = client.get("/users/" + params["user"] + "/tracks" )
-  	@user = client.get("/users/" + params["user"])
-  end
-
-  def track
-  	 @coordinates = Geocoder.search("west orange, nj").to_json
-  end
   
 end
