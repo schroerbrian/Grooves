91842240cfdb248227c15b1b3125cedab3a160bc
Brian Schroer <schroer.brian@gmail.com>
2013-07-23 23:00:31 -0700

    Add city model, edit city controller

---
+++ app/models/track.rb
@@ -0,0 +1,26 @@
+class Track
+	attr_accessor :get_tracks
+	
+	def initialize
+		@client = Soundcloud.new(:client_id => "e9e8fbf8ac2f57eb0f54519af9c2f22e")
+		@good_tracks = []
+	end
+
+	def get_tracks
+		tracks = @client.get('/tracks', :limit => 15)
+		tracks.each do |track|
+			user = @client.get('/users/' + track.user_id.to_s)
+				if user.city.present? && user.country.present?
+					geocoding = Geocoder.search("#{user.city}, #{user.country}").first
+					# track.delete_if {geocoding.nil?}
+					if geocoding
+						lat = geocoding.data["geometry"]["location"]["lat"] ? geocoding.data["geometry"]["location"]["lat"] : ['51']
+						lng = geocoding.data["geometry"]["location"]["lng"] ? geocoding.data["geometry"]["location"]["lng"] : ['0.1']
+					end
+					@good_tracks << { track: track, user: user, coordinates: [lat, lng] }
+				end
+		end
+		@good_tracks
+	end
+
+end
