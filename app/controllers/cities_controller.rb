9e5a14327faff0bf65166375823f556924cfcf98
Brian Schroer <schroer.brian@gmail.com>
2013-07-28 13:52:40 -0700

    change name to filtered_tracks. create table and fill with variable data

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,7 +2,7 @@ class CitiesController < ApplicationController
 
   def home
 		t = Track.new 
-		@good_tracks = t.get_tracks
+		@filtered_tracks = t.get_tracks
 	end
   
   def track
@@ -31,7 +31,27 @@ class CitiesController < ApplicationController
   end
 
   def test
+  	#our table instance
+  	
+  	#the tracks we call from the model
+  	t = Track.new 
+  	@filtered_tracks = t.get_tracks
+  	test_track_info = @filtered_tracks[0][:track]
+  	test_user_info = @filtered_tracks[0][:user]
+  	test_coordinates = @filtered_tracks[0][:coordinates]
+  	
+  	#putting our @filtered_tracks into the table 
+
+  	@table_test_track = TrackData.create(:track_id => test_track_info.id, 
+  		:track_name => test_track_info.title, :track_permalink => test_track_info.permalink,
+   		:user_id => test_user_info.id, :username => test_user_info.username, 
+   		:user_city => test_user_info.city, :user_country => test_user_info.country, 
+   		:user_coordinates => test_coordinates[0][:coordinates], :user_permalink => test_user_info.permalink, 
+			:user_avatar_url => test_user_info.avatar_url)
+
   	@track_data = TrackData.all
+
+
   end
 
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -3,12 +3,12 @@ class Track
 	
 	def initialize
 		@client = Soundcloud.new(:client_id => "e9e8fbf8ac2f57eb0f54519af9c2f22e")
-		@good_tracks = []
+		@filtered_tracks = []
 	end
 
 	#call api to get tracks
 	def get_tracks
-		tracks = @client.get('/tracks', :limit => 8)
+		tracks = @client.get('/tracks', :limit => 4)
 		tracks.each do |track|
 			user = @client.get('/users/' + track.user_id.to_s)
 				if user.city.present? && user.country.present?
@@ -18,10 +18,10 @@ class Track
 						lat = geocoding.data["geometry"]["location"]["lat"] ? geocoding.data["geometry"]["location"]["lat"] : ['51']
 						lng = geocoding.data["geometry"]["location"]["lng"] ? geocoding.data["geometry"]["location"]["lng"] : ['0.1']
 					end
-					@good_tracks << { track: track, user: user, coordinates: [lat, lng] }
+					@filtered_tracks << { track: track, user: user, coordinates: [lat, lng] }
 				end
 		end
-		@good_tracks
+		@filtered_tracks
 	end
 
 end
