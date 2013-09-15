2325c1d3ab602c280fe4579de328d07d69a8b216
Brian Schroer <schroer.brian@gmail.com>
2013-09-15 00:39:36 -0700

    Rake task is working

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,13 +2,13 @@ class Track
 	attr_accessor :get_database_tracks, :insert_tracks_into_track_data_table, :filtered_tracks
 	
 	def initialize
-		@client = Soundcloud.new(:client_id => "#{ENV['SOUNDCLOUD_KEY']}")
+		@client = Soundcloud.new(:client_id => "e9e8fbf8ac2f57eb0f54519af9c2f22e")
 		@filtered_tracks = []
 	end
 
 	#call api to get tracks; used for my home page
 	def get_tracks_from_soundcloud
-		tracks = @client.get('/tracks', :limit => 6)
+		tracks = @client.get('/tracks', :limit => 10)
 		tracks.each do |track|
 			user = @client.get('/users/' + track.user_id.to_s)
 				if user.city.present? && user.country.present?
@@ -25,19 +25,19 @@ class Track
 	end
 
 	#this will be a rake task
-	def insert_tracks_into_track_data_table
-  	@filtered_tracks.each do |filtered_track|
-  		if filtered_track[:coordinates].first.nil?
-  			filtered_tracks.delete(filtered_track)
-  		else 
-  			TrackData.create(:track_id => filtered_track[:track].id, 
-	  		:track_name => filtered_track[:track].title, :track_permalink => filtered_track[:track].permalink,
-	   		:user_id => filtered_track[:user].id, :username => filtered_track[:user].username, 
-	   		:user_city => filtered_track[:user].city, :user_country => filtered_track[:user].country, 
-	   		:user_permalink => filtered_track[:user].permalink, :user_avatar_url => filtered_track[:user].avatar_url,
-	   		:lat => filtered_track[:coordinates].first, :lng => filtered_track[:coordinates].last)
-  		end
-  	end
-	end
+	# def insert_tracks_into_track_data_table
+ #  	@filtered_tracks.each do |filtered_track|
+ #  		if filtered_track[:coordinates].first.nil?
+ #  			filtered_tracks.delete(filtered_track)
+ #  		else 
+ #  			TrackData.create(:track_id => filtered_track[:track].id, 
+	#   		:track_name => filtered_track[:track].title, :track_permalink => filtered_track[:track].permalink,
+	#    		:user_id => filtered_track[:user].id, :username => filtered_track[:user].username, 
+	#    		:user_city => filtered_track[:user].city, :user_country => filtered_track[:user].country, 
+	#    		:user_permalink => filtered_track[:user].permalink, :user_avatar_url => filtered_track[:user].avatar_url,
+	#    		:lat => filtered_track[:coordinates].first, :lng => filtered_track[:coordinates].last)
+ #  		end
+ #  	end
+	# end
 
 end
---
+++ lib/tasks/trackdata.rake
@@ -0,0 +1,54 @@
+
+namespace :trackdata do 
+	desc "import new tracks to the database"
+
+	task :current_environment do 
+		puts "You are pulling tracks from Soundcloud"
+	end 
+
+	task :import => :environment do
+
+		client = Soundcloud.new(:client_id => "e9e8fbf8ac2f57eb0f54519af9c2f22e")
+		tracks = client.get('/tracks', :limit => 200)
+		
+		downloaded_tracks = []
+		tracks.each do |track|
+			user = client.get('/users/' + track.user_id.to_s)
+				if user.city.present? && user.country.present?
+					geocoding = Geocoder.search("#{user.city}, #{user.country}").first
+				end
+				if geocoding
+					lat = geocoding.data["geometry"]["location"]["lat"]
+					lng = geocoding.data["geometry"]["location"]["lng"]
+					downloaded_tracks << { track: track, user: user, coordinates: [lat, lng] }
+				end 
+			downloaded_tracks
+		end 
+
+		puts downloaded_tracks
+
+		downloaded_tracks.each do |downloaded_track|
+			if downloaded_track[:coordinates].first.nil?
+				downloaded_track.delete(downloaded_track)
+			else 
+				t = TrackData.find_by_track_id(downloaded_track[:track].id)
+				if(t.nil?)
+					TrackData.create(
+						:track_id => downloaded_track[:track].id, 
+			  		:track_name => downloaded_track[:track].title, 
+			  		:track_permalink => downloaded_track[:track].permalink,
+			   		:user_id => downloaded_track[:user].id, 
+			   		:username => downloaded_track[:user].username, 
+			   		:user_city => downloaded_track[:user].city, 
+			   		:user_country => downloaded_track[:user].country, 
+			   		:user_permalink => downloaded_track[:user].permalink, 
+			   		:user_avatar_url => downloaded_track[:user].avatar_url,
+			   		:lat => downloaded_track[:coordinates].first, 
+			   		:lng => downloaded_track[:coordinates].last
+			   		)
+				end 
+			end 
+		end 
+		
+	end 
+end 	
